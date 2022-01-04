0ec5223ea2a5ae8f365b4ac656c5202099029a35
edgarsilva <edgar.osc@gmail.com>
2022-01-03 19:06:35 -0600

    Remove bg color from body.

--- client/src/services/firebase.js
+++ client/src/services/firebase.js
@@ -9,14 +9,7 @@ import "firebase/auth";
 
 // Firebase setup
 !firebase.apps.length && firebase.initializeApp({
-  apiKey: "AIzaSyAl0gOF_KoCSk9VQb_jqClM6H26fl_PABA",
-  authDomain: "restoration-md.firebaseapp.com",
-  databaseURL: "https://restoration-md.firebaseio.com",
-  projectId: "restoration-md",
-  storageBucket: "restoration-md.appspot.com",
-  messagingSenderId: "705185549782",
-  appId: "1:705185549782:web:0b73a05d1d8b700b17e877",
-  measurementId: "G-PK6JGKS3KV"
+
 });
 
 // Auth
